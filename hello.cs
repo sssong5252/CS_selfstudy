bae75184d23ba6c3643ec722e9965257d3b73c32
sssong5252 <luckyjude@naver.com>
2024-03-16 10:45:27 +0900

    0316

--- hello.cs
+++ hello.cs
@@ -1,25 +1,39 @@
 using System;
-namespace hello {
-class HelloWorld {
-   static void Main(string[] args) {
-      Console.WriteLine("hellowol");
-      int number;
-      number = 6;
-      Console.WriteLine(number);
 
-      if(number == 7) {
-         Console.WriteLine("hello number is 7");
-      }
-      while(true) {
-         if(number == 7) {
-            Console.WriteLine("this code will return false");
-            break;
-         }
-         else {
-            Console.WriteLine("num is not 7");
-            break;
-         }
-      }
-   }
- }
+namespace hello {
+    class HelloWorld {
+        static void change(ref int x, ref int y) { //ref는 값을 전달하기 위해 쓰인다. 콜바이레퍼런스?? 그거 말하는 것 같음
+            int tmp;
+            tmp = x;
+            x = y;
+            y = tmp;
+        }
+
+        static void Main(string[] args) {
+            Console.WriteLine("hellowol");
+            int number;
+            number = 6;
+            Console.WriteLine(number);
+
+            if (number == 7) {
+                Console.WriteLine("hello number is 7");
+            }
+
+            while (true) {
+                if (number == 7) {
+                    Console.WriteLine("this code will return false");
+                    break;
+                } else {
+                    Console.WriteLine("num is not 7");
+                    break;
+                }
+            }
+
+            int a = 10;
+            int b = 0;
+            change(ref a, ref b);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
 }
