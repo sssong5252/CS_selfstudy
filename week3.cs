7781f7dab8aea765edd91ac6678395054d2f0fc2
sssong5252 <luckyjude@naver.com>
2024-04-05 11:39:03 +0900

    0405

--- week3.cs
+++ week3.cs
@@ -2,7 +2,39 @@ using System;
 
 namespace week3 {
     class studyweek3 {
-        static void main(string[] args) {
+        static void Main(string[] args) {
+            // int number = 10;
+            // Console.WriteLine(number);
+            // Console.WriteLine(number++);
+            // Console.WriteLine(number--);
+            // Console.WriteLine(number);
+            
+            // Console.WriteLine(number);
+            // Console.WriteLine(++number);
+            // Console.WriteLine(--number);
+            // Console.WriteLine(number);
+
+
+            // int output = 0;
+            // output += 52;
+            // output += 273;
+            // output += 103;
+            // System.Console.WriteLine(output);
+
+            // string output = "hello";
+            // output += world;
+            // output += !;
+            // System.Console.WriteLine(output);
+
+            // int number;
+            // number++;
+            // System.Console.WriteLine(number);
+            // number--;
+            // System.Console.WriteLine(number);
+
+            string input = Console.ReadLine();
+            System.Console.WriteLine(input);
+
             
         }
     }
---
+++ week4.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Name
+{
+    class week4{
+        static void Main(string[] args) {
+            double number = 52.273103;
+            Console.WriteLine(number.ToString("0.0"));
+            Console.WriteLine(number.ToString("0.00"));
+            Console.WriteLine(number.ToString("0.000"));
+            Console.WriteLine(number.ToString("0.0000"));
+            System.Console.WriteLine("20230850_2학년_001_송준용");
+        }
+    }
+}
