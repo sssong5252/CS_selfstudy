5d53db0147922502f8c896c659bfe9b2e89d01e9
sssong5252 <luckyjude@naver.com>
2024-06-07 12:37:20 +0900

    0607

--- class_study.cs
+++ class_study.cs
@@ -1,9 +1,22 @@
 using System;
 
-namespace Car_class {
-    class cs {
-        static void Main(String[] args) {
-            
-        }
+namespace MethodWithOut
+{
+    static void NextPosition(int x, int y, int vx, int vy, out int rx, out int ry)
+    {
+        rx = x + vx;
+        ry = y + vy;
+    }
+    static void Main(string[] args) 
+    {
+        int x = 0;
+        int y = 0;
+        int vx = 1;
+        int vy = 1;
+
+        System.Console.WriteLine("현재 좌표: ");
+        NextPosition(x, y, vx, vy,out x, out y);
+        System.Console.WriteLine("");
+
     }
 }
